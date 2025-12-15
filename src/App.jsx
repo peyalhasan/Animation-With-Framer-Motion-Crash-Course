e66cd2104ec057fad25280cd295652807586c029
Peyal Hasan <peyalhasan143@gmail.com>
2025-12-16 00:14:02 +0600

    Framer Motion

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Simple from './Components/Simple'
+
+function App() {
+
+  return (
+    <>
+      <Simple />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Keyframe.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Keyframe = () => {
+    return (
+        <motion.div
+        
+        />
+
+        
+    );
+};
+
+export default Keyframe;
